e6e5ad4ac61d9acd6db95ac37992290552bd45ac
theanh098 <mtheanh2910@gmail.com>
2023-09-21 22:04:49 +0700

    fmt

--- src/shared/errors/common/ExecutionError.ts
+++ src/shared/errors/common/ExecutionError.ts
@@ -6,6 +6,11 @@ export const executionErrorTag: unique symbol = Symbol(
   "executionContextErrorTag"
 );
 
+/**
+ * Represent to any error.
+ * Not recommended.
+ * Let's Define specific error in your context instead.
+ */
 export type ExecutionError = Readonly<{
   _tag: typeof executionErrorTag;
   reason: Error;
