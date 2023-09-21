a9b5fd83215c738b0b0367e539f7d4004f5c23f1
theanh098 <mtheanh2910@gmail.com>
2023-09-21 22:05:49 +0700

    f

--- src/shared/errors/common/ExecutionError.ts
+++ src/shared/errors/common/ExecutionError.ts
@@ -2,24 +2,22 @@ import * as E from "fp-ts/Either";
 
 import type { AnyHow } from "../adapter";
 
-export const executionErrorTag: unique symbol = Symbol(
-  "executionContextErrorTag"
-);
+export const executionErrorTag: unique symbol = Symbol("executionErrorTag");
 
 /**
  * Represent to any error.
  * Not recommended.
- * Let's Define specific error in your context instead.
+ * Let's define specific error in your context instead.
  */
 export type ExecutionError = Readonly<{
   _tag: typeof executionErrorTag;
   reason: Error;
 }>;
 
-export const executionContextError = (e: unknown): ExecutionError => ({
+export const executionError = (e: unknown): ExecutionError => ({
   _tag: executionErrorTag,
   reason: E.toError(e)
 });
 
-export const isExecutionContextError = (err: AnyHow): err is ExecutionError =>
+export const isExecutionError = (err: AnyHow): err is ExecutionError =>
   err._tag === executionErrorTag;
